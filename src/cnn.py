b7040e662120a75de107dc85f03a6adde3903583
Han Zhang <hanzhan@microsoft.com>
2018-03-27 14:24:02 -0700

    add rnn

--- src/cnn.py
+++ src/cnn.py
@@ -11,22 +11,31 @@ import models
 class CNN(models.BaseModel):
     def __init__(self,model):
         self.base_init(model)
-        self.hidden_sizes = [128, 256]
+        self.kernel_sizes=[2,3,4]
+        self.num_filters=2
+        self.dropout_keep_prob = 1.0
 
-    def create_actual_model(self, seq):
-        layers = [tf.nn.rnn_cell.GRUCell(size) for size in self.hidden_sizes]
-        cells = tf.nn.rnn_cell.MultiRNNCell(layers)
-        batch = tf.shape(seq)[0]
-        zero_states = cells.zero_state(batch, dtype=tf.float32)
-        self.in_state = tuple([tf.placeholder_with_default(state, [None, state.shape[1]])
-                               for state in zero_states])
-        # this line to calculate the real length of seq
-        # all seq are padded to be of the same length, which is num_steps
-        length = tf.reduce_sum(tf.reduce_max(tf.sign(seq), 2), 1)
-        self.output, self.out_state = tf.nn.dynamic_rnn(cells, seq, length, self.in_state)
+
+    def create_actual_model(self, embd):
+        pooled_outputs=[]
+        for i,kernel_size in enumerate(self.kernel_sizes):
+            with tf.name_scope("cnn_max_pool-%s" % kernel_size):
+                kernel_shape=[kernel_size,self.embedding_size,1,self.num_filters]
+                W = tf.Variable(tf.truncated_normal(kernel_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[self.num_filters]), name="b")
+                conv = tf.nn.conv2d(tf.expand_dims(embd,-1),W,strides=[1,1,1,1],padding='VALID',name='conv')
+                h = tf.nn.relu(tf.nn.bias_add(conv,b),name='relu')
+                pooled = tf.nn.max_pool(h,ksize=[1,self.num_steps-kernel_size+1,1,1],strides=[1,1,1,1],padding='VALID',name='max_pooled')
+                pooled_outputs.append(pooled)
+        num_filters_total = self.num_filters*len(self.kernel_sizes)
+        h_pool = tf.concat(pooled_outputs,3)
+
+        with tf.name_scope('dropouts'):
+            h_pool_drop = tf.nn.dropout(tf.reshape(h_pool,[-1,num_filters_total]),self.dropout_keep_prob)
+        self.output = h_pool_drop
 
     def get_logits(self):
-        self.logits = tf.layers.dense(self.out_state[len(self.hidden_sizes) - 1], self.num_classes, None)
+        self.logits = tf.layers.dense(self.output, self.num_classes, None)
 
 
 
--- src/rnn.py
+++ src/rnn.py
@@ -28,17 +28,18 @@ class RNN(models.BaseModel):
         self.hidden_sizes = [128, 256]
 
     def create_actual_model(self, embd):
-        layers = [tf.nn.rnn_cell.GRUCell(size) for size in self.hidden_sizes]
-        cells = tf.nn.rnn_cell.MultiRNNCell(layers)
-        batch = tf.shape(embd)[0]
-        zero_states = cells.zero_state(batch, dtype=tf.float32)
-        self.in_state = tuple([tf.placeholder_with_default(state, [None, state.shape[1]])
-                               for state in zero_states])
+        with tf.name_scope("rnn_cell"):
+            layers = [tf.nn.rnn_cell.GRUCell(size) for size in self.hidden_sizes]
+            cells = tf.nn.rnn_cell.MultiRNNCell(layers)
+            batch = tf.shape(embd)[0]
+            zero_states = cells.zero_state(batch, dtype=tf.float32)
+            self.in_state = tuple([tf.placeholder_with_default(state, [None, state.shape[1]])
+                                   for state in zero_states])
 
-        # this line to calculate the real length of seq
-        # all seq are padded to be of the same length, which is num_steps
-        length = tf.reduce_sum(tf.reduce_max(tf.sign(embd), 2), 1)
-        self.output, self.out_state = tf.nn.dynamic_rnn(cells, embd, length, self.in_state)
+            # this line to calculate the real length of seq
+            # all seq are padded to be of the same length, which is num_steps
+            length = tf.reduce_sum(tf.reduce_max(tf.sign(embd), 2), 1)
+            self.output, self.out_state = tf.nn.dynamic_rnn(cells, embd, length, self.in_state)
 
     def get_logits(self):
         self.logits = tf.layers.dense(self.out_state[len(self.hidden_sizes) - 1], self.num_classes, None)
