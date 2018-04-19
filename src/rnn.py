1a4f66018c47fb557e82d640ac67289ebc1cee8d
Han Zhang <hanzhan@microsoft.com>
2018-04-18 17:29:19 -0700

    add

--- src/rnn.py
+++ src/rnn.py
@@ -42,8 +42,11 @@ class LSTM(RNN):
             batch = tf.shape(embd)[0]
             zero_states = cells.zero_state(batch, dtype=tf.float32)
 
-            self.in_state = tuple([tf.placeholder_with_default(state, [None, state.shape[1]])
-                                   for state in zero_states])
+            #self.in_state = tuple([tf.placeholder_with_default(state, [None, state.shape[1]])
+            #                       for state in zero_states])
+
+            self.in_state = tuple(([tf.placeholder_with_default(state, ([None, state.c.shape[1]],[None, state.h.shape[1]]),([None, state.c.shape[1]],[None, state.h.shape[1]]))
+                                   for state in zero_states]))
 
             # this line to calculate the real length of seq
             # all seq are padded to be of the same length, which is num_steps
