0bfb4633805afba8e592792175be01865f0f725d
‘michaelzhhan1 <hanz@cs.cmu.edu>
2018-04-29 23:42:50 -0700

    start Han model

---
+++ src/Hierarchical_Attention.py
@@ -0,0 +1,11 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import sys
+sys.path.append('..')
+import tensorflow as tf
+import models
+import config
+import rnn
+
+class HAN(rnn.RNN):
+    
