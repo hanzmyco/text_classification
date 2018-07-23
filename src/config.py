44f8741546213d187a2803cfb9a5eed362da4ba4
mycohzhang(张涵) <mycohzhang@tencent.com>
2018-07-23 18:49:06 +0800

    modify for yooutu

--- src/config.py
+++ src/config.py
@@ -1,24 +1,23 @@
 
 # parameters for processing the dataset
-DATA_PATH = '../data/kaggle/'
+DATA_PATH = '../../../data/caption_data/'
 TRAIN_DATA_NAME= 'train.txt'
 TRAIN_DATA_NAME_PROCESSED= 'train.txt.ids'
-TRAIN_LABEL_NAME= 'labels.txt'
+TRAIN_LABEL_NAME= 'toy_classes.txt'
 
 #INFERENCE_DATA_NAME ='train.txt'
 INFERENCE_DATA_NAME_PROCESSED ='train.txt.ids'
 INFERENCE_LABEL_NAME='labels.txt'
 INFERENCE_RESULT_NAME='labels_result_test.txt'
 
-PROCESSED_PATH = '../data/Processed/'
+PROCESSED_PATH = '../../../data/caption_data/Processed/'
 
-#MODEL_NAME='CNN'
+MODEL_NAME='CNN'
 #MODEL_NAME='GRU'
-MODEL_NAME='LSTM'
+#MODEL_NAME='LSTM'
 
-MODEL_BI_DIRECTOion = True
 
-CPT_PATH = '../checkpoints/'+MODEL_NAME
+CPT_PATH = '../../../data/caption_data/checkpoints/'+MODEL_NAME
 
 VOCAB_SIZE=15217
 UNK_ID = 0
@@ -34,7 +33,7 @@ HIDDEN_SIZE = [128,256]
 BATCH_SIZE = 64
 LR = 0.0003
 #NUM_SAMPLES = 512
-NUM_CLASSES=5
+NUM_CLASSES=8
 EMBEDDING_SIZE=256
 KERNEL_SIZE=[2,3,4]
 NUM_FILTERS=2
@@ -42,7 +41,7 @@ DROPOUT_KEEP_PROB=1.0
 
 READ_IN_FORMAT=[[0]]*(NUM_STEPS)
 ONE_HOT_TAG=False
-EPOCH_NUM = 500
+EPOCH_NUM = 1000
 
 PRETRAIN_EMBD_TAG=False
 PRETRAIN_EMBD_TRAINABLE=False
